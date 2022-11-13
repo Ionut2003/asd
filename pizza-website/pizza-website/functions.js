90c1ddec5fbbcff3504d2992408147a89c31d6be
Ionut2003 <65741061+Ionut2003@users.noreply.github.com>
2022-11-13 08:15:44 -0800

    Add files via upload

---
+++ pizza-website/pizza-website/functions.js
@@ -0,0 +1,16 @@
+const buttons = document.getElementsByClassName('button');
+buttons[0].addEventListener('click', (e) =>{
+    window.location.href = "pizza.html";    
+})
+buttons[1].addEventListener('click', (e) =>{
+    window.location.href = "paste.html";    
+})
+buttons[2].addEventListener('click', (e) =>{
+    window.location.href = "salate.html";    
+})
+buttons[3].addEventListener('click', (e) =>{
+    window.location.href = "desert.html";    
+})
+buttons[4].addEventListener('click', (e) =>{
+    window.location.href = "bauturi.html";    
+})
